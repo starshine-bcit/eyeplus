beaf18d4fee7b65fc0037b6cf442483903b89538
liliu <sfey1@my.bcit.ca>
2023-02-06 09:56:44 -0800

    Fix path environment so it will only be added once

--- src/eyeplus.py
+++ src/eyeplus.py
@@ -2,7 +2,9 @@ import os
 from pathlib import Path
 import sys
 
-os.environ["PATH"] = str(Path(__file__).parent.parent / 'mlib') + os.pathsep + os.environ["PATH"]
+mlib_path = str(Path(__file__).parent.parent / 'mlib')
+if mlib_path not in os.environ['PATH']:
+    os.environ['PATH'] = str(Path(__file__).parent.parent / 'mlib') + os.pathsep + os.environ['PATH']
 
 from qt.mainwindow import EyeMainWindow
 from PyQt6 import QtWidgets
