1736c634fb63a6957935d19443e242a5688f8ee9
Parsa Momeni <90473107+parsamomeni2001@users.noreply.github.com>
2023-02-08 19:51:29 -0800

    worked on calculate _horizon_line function

--- src/utils/algorithm.py
+++ src/utils/algorithm.py
@@ -1,6 +1,20 @@
+import math
+
 def calculate_horizon_line(accelerometer, gyroscope):
     """Receives accelerometer and gyroscope data and returns the angle and position of the horizon line."""
-    pass
+
+    #Calculates the pitch angle using the accelerometer data
+    pitch = math.atan2(-accelerometer[0], math.sqrt(accelerometer[1]**2+ accelerometer[2]**2))
+
+    #Integrates the angular velocity from the gyroscope data to get the roll angle
+    roll = 0
+    for i in range(1, len(gyroscope)):
+        roll += gyroscope[i-1] * (gyroscope[i][0]-gyroscope[i-1][0]) 
+
+
+    #Returns the pitch and rolls angles
+    return pitch, roll    
+    
 
 def compare_gaze_coordinates(gaze_coordinates):
     """Uses horizon line angle and position and gaze coordinates to determine if the user is looking above or below the horizon at the timestamp."""
