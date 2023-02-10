a4c2fa537c11e019916fc21a3c5cebe3df5ff7f7
John Doe <balcerak12@gmail.com>
2023-02-09 21:07:07 -0800

    create calculations for x  y intercepts and slope

--- src/utils/algorithm.py
+++
@@ -1,7 +0,0 @@
-def calculate_horizon_line(accelerometer, gyroscope):
-    """Receives accelerometer and gyroscope data and returns the angle and position of the horizon line."""
-    pass
-
-def compare_gaze_coordinates(gaze_coordinates):
-    """Uses horizon line angle and position and gaze coordinates to determine if the user is looking above or below the horizon at the timestamp."""
-    pass
