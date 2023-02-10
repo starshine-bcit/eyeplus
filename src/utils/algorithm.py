bb812c2625a65eb2cbf60597cedb75ae91b0f521
Parsa Momeni <90473107+parsamomeni2001@users.noreply.github.com>
2023-02-09 21:32:34 -0800

    made a few changes to calculate_horizon_line

--- src/utils/algorithm.py
+++ src/utils/algorithm.py
@@ -2,19 +2,17 @@ import math
 
 def calculate_horizon_line(accelerometer, gyroscope):
     """Receives accelerometer and gyroscope data and returns the angle and position of the horizon line."""
+    # Calculates the roll angle using the gyroscope data
+    roll = gyroscope[0]
 
-    #Calculates the pitch angle using the accelerometer data
-    pitch = math.atan2(-accelerometer[0], math.sqrt(accelerometer[1]**2+ accelerometer[2]**2))
+    # Calculates the pitch angle using the accelerometer and gyroscope data
+    pitch = math.atan2(-accelerometer[0], math.sqrt(accelerometer[1]**2 + accelerometer[2]**2))
+    pitch_gyro = gyroscope[1]
 
-    #Integrates the angular velocity from the gyroscope data to get the roll angle
-    roll = 0
-    for i in range(1, len(gyroscope)):
-        roll += gyroscope[i-1] * (gyroscope[i][0]-gyroscope[i-1][0]) 
+    # Integrates the pitch angle over time to estimate the current angle of the device
+    pitch = pitch * 0.98 + pitch_gyro * 0.02
 
-
-    #Returns the pitch and rolls angles
-    return pitch, roll    
-    
+    return roll, pitch
 
 def compare_gaze_coordinates(gaze_coordinates):
     """Uses horizon line angle and position and gaze coordinates to determine if the user is looking above or below the horizon at the timestamp."""
