61b09663d0ac2f53832bb5c0249f9b7e146a5a82
John Doe <balcerak12@gmail.com>
2023-02-16 19:27:25 -0800

    created analyze method for checking result while testing

--- src/modules/analyze.py
+++ src/modules/analyze.py
@@ -8,21 +8,31 @@ class Analyze:
         self.readings_looking_down = 0
         self.total_readings = 0
 
-    def check(self, y_intercept, slope, gaze2d_x, gaze2d_y, gaze3d_distance_from_user):
+    def check(self, slope, y_intercept, gaze_x, gaze_y, gaze_dist):
         """Performs the check to see if a user is looking up or down and increments the number of checks that have been made."""
         
         #gaze3d_distance_from_user is the third element in the gaze3d dictionary
         self.total_readings += 1
-        if gaze2d_y > slope * gaze2d_x + y_intercept:
+        if gaze_y > slope * gaze_x + y_intercept:
             self.readings_looking_up += 1
-        elif gaze3d_distance_from_user > 400:
+        elif gaze_dist > 400:
             self.readings_looking_up += 1
         else:
             self.readings_looking_down += 1
 
+    def test_check(slope, y_intercept, gaze_x, gaze_y, gaze_dist = 0):
+        print(gaze_dist)
+        """Returns a string describing whether a user is looking up or down with some data, for testing purposes."""
+        if gaze_y > slope * gaze_x + y_intercept:
+            return "Looking Up"
+        elif gaze_dist > 400:
+            return "Looking Up"
+        else:
+            return "Looking Down"
+
     def calculate(self):
         """Returns a tuple containing the percentages of time spent looking up and looking down."""
-        
+
         percent_looking_up = self.readings_looking_up / self.total_readings
         percent_looking_down = self.readings_looking_down / self.total_readings
 
