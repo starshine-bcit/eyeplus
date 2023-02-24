e3fc1d510145d3b0ec0eb909d0f979508da7e6c5
Dhruv Dhawan <dhruvdhawan11@gmail.com>
2023-02-24 01:06:25 -0800

    export csv data

--- src/modules/export.py
+++ src/modules/export.py
@@ -7,9 +7,9 @@ from modules.eyedb import EyeDB
 
 class DataExporter:
     def __init__(self, eyedb: EyeDB) -> None:
-        """This object will grab data from the database and write out two csv
-        files, one with the predicted 2d gaze data, the other with sensor fusion data,
-        either for one run, or all runs.
+        """This object will grab data from the database and write out three csv
+        files, one with the predicted 2d gaze data, one with sensor fusion data,
+        and one with processed data, either for one run, or all runs.
 
         Args:
             eyedb (EyeDB): The database object to interact with.
@@ -23,8 +23,13 @@ class DataExporter:
         pgaze = self._db.get_pgazed2d_data(runid)
         pgaze = [{'runid': runid, 'timestamp': k,
                   'pgaze2dx': v[0], 'pgaze2dy': v[1]} for k, v in pgaze.items()]
+        processed = self._db.get_processed_data(runid)
+        processed = [{'runid': runid, 'timestamp': p['timestamp'],
+                      'totalcount': p['totalcount'], 'upcount': p['upcount'], 'downcount': p['downcount'],
+                      'percentup': p['percentup'], 'percentdown': p['percentdown'], 'currentup': p['currentup']} for p in processed]
         fused_out = out_path.with_name(f'{out_path.stem}_fusion.csv')
         pgaze_out = out_path.with_name(f'{out_path.stem}_pgaze.csv')
+        processed_out = out_path.with_name(f'{out_path.stem}_processed.csv')
         with fused_out.open('w', encoding='utf8', newline='') as fo:
             csv_writer = csv.DictWriter(fo, fieldnames=list(fused[0].keys()))
             csv_writer.writeheader()
@@ -33,6 +38,10 @@ class DataExporter:
             csv_writer = csv.DictWriter(fo, fieldnames=list(pgaze[0].keys()))
             csv_writer.writeheader()
             csv_writer.writerows(pgaze)
+        with processed_out.open('w', encoding='utf8', newline='') as fo:
+            csv_writer = csv.DictWriter(fo, fieldnames=list(processed[0].keys()))
+            csv_writer.writeheader()
+            csv_writer.writerows(processed)
 
     def export_all(self, out_path: Path) -> None:
         all_runs = self._db.get_all_runs()
